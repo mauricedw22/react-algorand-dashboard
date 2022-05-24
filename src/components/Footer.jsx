a16248f70b1bf5738bb6b9044b6f01f40e7d7358
Maurice WALKER <mauricedw22@gmail.com>
2022-05-24 16:20:55 -0400

    1st commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+
+    return (
+        <footer class="foot bg-orange-400 text-center lg:text-left">
+            <div class="text-white text-center p-4">
+                ©  Copyright 2022 &nbsp;
+                <a class="text-gray-800" href="https://tailwind-elements.com/">YPW</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import AlgoLogo from '../assets/algorand_full_logo_white.png';
+import './Navbar.css';
+
+const Navbar = () => {
+
+    return (
+        <React.Fragment>
+            <nav className="navbar navbar-expand-lg shadow-lg py-2 bg-orange-400 relative flex items-center w-full justify-between">
+                <div className="px-6">
+                    <button className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-white hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContentY" aria-controls="navbarSupportedContentY" aria-expanded="false" aria-label="Toggle navigation">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
+                    </svg>
+                    </button>
+                    <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
+                        <img className="logo" src={AlgoLogo} width="150" height="75" />
+                        <ul className="navbar-nav mr-auto flex flex-row">
+                            <li className="nav-item">
+                                <a className="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                                    On-Chain Data
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </React.Fragment>        
+    );
+} 
+
+export default Navbar
