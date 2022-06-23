6ea963b3ac07aaec9adfc6d22fd2dfd4879be29b
Maurice WALKER <mauricedw22@gmail.com>
2022-06-23 19:20:15 -0400

    eb edits

--- src/aws/putEvent.js
+++ src/aws/putEvent.js
@@ -1,10 +1,10 @@
 import { PutEventsCommand } from "@aws-sdk/client-eventbridge";
-import { ebClient } from "./ebClient";
+import { ebClient } from "./ebClient.js";
 
 export const params = {
     Entries: [
         {
-            Detail: '{"key1":"value1","key2":"value2"}',
+            Detail: '{"key1":"value1","key2":"value2","married":"true"}',
             DetailType: "appEventCreated",
             Resources:[
                 "", // RESOURCE_ARN
@@ -17,7 +17,7 @@ export const params = {
 export const run = async() => {
     try {
         const data = await ebClient.send(new PutEventsCommand(params));
-        console.log("Success - Event sent! Requiest id: ", data);
+        console.log("Success - Event sent! Request id: ", data);
         return data;
     } catch (err) {
         console.log("Error", err);
